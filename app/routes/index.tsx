07ae82646105ef0c3f17c24bfba6f6ac729840b1
Brad Garropy <bgarropy@atlassian.com>
2023-02-07 23:14:18 -0600

    wire input to squares.

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,49 @@
 import type {MetaFunction} from "@remix-run/node"
+import type {FormEventHandler} from "react"
+import {useState} from "react"
+
+import {assignSquares} from "~/utils/assign"
 
 const meta: MetaFunction = () => ({
     title: "💿 remix starter | home",
 })
 
 const IndexRoute = () => {
-    return <h2 className="text-2xl font-bold">Home</h2>
+    const [squares, setSquares] = useState<string[]>([])
+
+    const onSubmit: FormEventHandler<HTMLFormElement> = event => {
+        event.preventDefault()
+
+        const formData = new FormData(event.currentTarget)
+        const rawNames = formData.get("names") as string
+        const names = rawNames.split(",").map(name => name.trim())
+
+        const newSquares = assignSquares(names)
+        setSquares(newSquares)
+    }
+
+    return (
+        <>
+            <form onSubmit={onSubmit}>
+                <label htmlFor="names">Names</label>
+
+                <input
+                    type="text"
+                    name="names"
+                    id="names"
+                    className="text-black"
+                />
+
+                <button type="submit">generate</button>
+            </form>
+
+            <div className="grid grid-cols-10">
+                {squares.map((square, index) => {
+                    return <p key={index}>{square}</p>
+                })}
+            </div>
+        </>
+    )
 }
 
 export default IndexRoute
