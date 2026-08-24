4e422e250806e47a54377822d225fa75beb2c881
Brad Garropy <bradgarropy@gmail.com>
2026-08-24 15:51:32 -0500

    define routes.

---
+++ src/routes.ts
@@ -0,0 +1,15 @@
+import {index, route} from "@react-router/dev/routes"
+import {type RouteConfig} from "@react-router/dev/routes"
+
+const routes: RouteConfig = [
+    index("routes/_index.tsx"),
+    route("account", "routes/account.tsx"),
+    route("boards", "routes/boards._index.tsx"),
+    route("boards/new", "routes/boards.new.tsx"),
+    route("boards/:id", "routes/boards.$id.tsx"),
+    route("login", "routes/login.tsx"),
+    route("logout", "routes/logout.tsx"),
+    route("signup", "routes/signup.tsx"),
+]
+
+export default routes
