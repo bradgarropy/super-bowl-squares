4ff9eb99307671b7172c33f02cea9539f1391662
Brad Garropy <bradgarropy@gmail.com>
2026-08-26 21:11:43 -0500

    ☁️ cloudflare (#4)
    * ignore generated cloudflare files.
    
    * install cloudflare.
    
    * add wrangler configuration.
    
    * build with cloudflare vite plugin.
    
    * add workers boilerplate.
    
    * trigger cloudflare build
    
    * trigger cloudflare build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+import {cloudflare} from "@cloudflare/vite-plugin"
 import {reactRouter} from "@react-router/dev/vite"
 import {defineConfig} from "vite"
 
 const config = defineConfig({
-    plugins: [reactRouter()],
+    plugins: [cloudflare({viteEnvironment: {name: "ssr"}}), reactRouter()],
     resolve: {
         tsconfigPaths: true,
     },
