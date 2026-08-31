2ac38c0fc3e1eaf9cf1fe347a08f464c44337297
Brad Garropy <bradgarropy@gmail.com>
2026-08-30 21:40:02 -0500

    protect account route.

--- src/utils/auth.server.ts
+++ src/utils/auth.server.ts
@@ -2,6 +2,7 @@ import {drizzleAdapter} from "@better-auth/drizzle-adapter"
 import {betterAuth} from "better-auth/minimal"
 import {env} from "cloudflare:workers"
 import {drizzle} from "drizzle-orm/d1"
+import {redirect} from "react-router"
 
 import * as schema from "~/db/auth"
 
@@ -25,4 +26,14 @@ const auth = betterAuth({
     },
 })
 
-export {auth}
+const requireUser = async (request: Request) => {
+    const session = await auth.api.getSession({headers: request.headers})
+
+    if (!session) {
+        throw redirect("/login")
+    }
+
+    return session.user
+}
+
+export {auth, requireUser}
