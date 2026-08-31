2557e3609ed1fe8ca3170b731f615a3b60d0f32f
Brad Garropy <bradgarropy@gmail.com>
2026-08-30 21:12:54 -0500

    remove session utils.

--- src/utils/session.server.ts
+++
@@ -1,9 +0,0 @@
-import {createCookieSessionStorage} from "react-router"
-
-import {cookie} from "~/utils/cookie.server"
-
-const {commitSession, destroySession, getSession} = createCookieSessionStorage({
-    cookie,
-})
-
-export {commitSession, destroySession, getSession}
